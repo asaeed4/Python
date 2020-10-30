0cc20f468926b742d6021bfefe5bce8f96e82a51
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2020-10-30 12:37:06 -0500

    push 5

---
+++ Comments/commentDrills.py
@@ -0,0 +1,30 @@
+'''
+Created on Oct 30, 2020
+
+@author: ITAUser
+'''
+#1) Make a single line comment.
+#single line comment
+
+
+#2) Make a multi-line comment.
+'''
+multi-line
+comment
+'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+carOne=5 #single line comment
+
+
+#4) Make a single line comment.
+#you could fix 'this' to remove the error
+
+
+#5) Make a multi-line comment.
+'''
+replace 'this' 
+with 'this'
+to remove the syntax error
+'''
