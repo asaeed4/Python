70e620746b6d0e91a21dedcdc96d342f46350448
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2020-10-30 12:23:23 -0500

    push 4

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 30, 2020
+
+@author: ITAUser
+'''
+#1) Make a single line comment.
+#This is an example of a single line comment
+
+#2) Make a multi-line comment.
+'''This 
+is a 
+multi-line
+comment'''
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+varA=5 #comment goes here
