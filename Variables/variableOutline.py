f1a6a640dbebcaad7126c7b6898b6e593778423b
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2020-10-30 00:05:19 -0500

    push 1

---
+++ Variables/variableOutline.py
@@ -0,0 +1,71 @@
+'''
+Created on Oct 28, 2020
+
+@author: ITAUser
+'''
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+from _csv import Error
+
+#Int
+a=5 
+
+#Float
+a=3.65
+
+#String 
+
+a="string"
+name="Ahmed Saeed" 
+
+#Boolean
+
+a=True 
+a=False 
+
+#List
+
+a=[1.0, 1.1, 1.2, 1.3]
+a=["stringOne","stringTwo"] 
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+
+carA=3
+cara=2
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+cat=43
+cat="fourtyFive"
+
+#4)
+#Make a variable and set it to a variable you have already created:
+care=cat
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+listOne=["x","y","z"]
+carB=(listOne[1])
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+name=["Ahmed","Saeed"]
+carC=(name[0])
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+print(carB)
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+#Logical Error: forgetting to set a variable
+water=2
+carFour=water
