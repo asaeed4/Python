8c254b91270ea02696bdbaf8fc293bdad37478a1
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2020-11-03 17:15:39 -0600

    push 6.2

---
+++ Input/inputOutline.py
@@ -0,0 +1,25 @@
+'''
+Created on Nov 3, 2020
+
+@author: ITAUser
+'''
+#1) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+text_to_user="What is your favorite color?"
+favColor=input(text_to_user)
+
+print(favColor)
+#2) Make a variable called question and set it to "What is your name?". Then
+#get input from the user to the question and store their answer in a variable
+#called name.
+question="What is your name?"
+name=input(question)
+
+print(name)
+#3) Get input from the user to the question "What is your age?" and store their
+#answer in a variable called age. Convert age from a string variable to a int
+#(integer) variable.
+text="What is your age?"
+age=input(text)
+
+print(age)
