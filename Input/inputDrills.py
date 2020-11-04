54b9a77de6f142a1576dcd0cf002ab10a6cce7ae
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2020-11-03 19:13:52 -0600

    psuh 6.3

---
+++ Input/inputDrills.py
@@ -0,0 +1,35 @@
+'''
+Created on Nov 3, 2020
+
+@author: ITAUser
+'''
+#1) Get input from the user to the question "What is your favorite animal?"
+#and store their answer in a variable called favAnimal.
+question="What is your favorite animal?"
+favAnimal=input(question)
+print(favAnimal)
+
+#2) Get input from the user to the question "What is your favorite number?"
+#and store their answer in a variable called favNum. Convert favNum from a 
+#string variable to a int variable.
+query="What is your favorite number?"
+favNum=input(query)
+print(favNum)
+
+#3) Get input from the user to the question "What is your school's name?"
+#and store their answer in a variable called school.
+question="What is your school's name?"
+school=input(question)
+print(school)
+#4) Get input from the user to the question "What is your instructors name?"
+#and store their answer in a variable called instructor.
+question="What is your instructors name?"
+instructor=input(question)
+print(instructor)
+
+#5) Get input from the user to the question "What is 2 + 2?"
+#and store their answer in a variable called ans. Convert ans from a string
+#variable to a int variable
+question="What is 2+2?"
+ans=input(question)
+print(ans)
