4025021d536e675d21edf373e3244ae199a5fbd0
asaeed4 <73620645+asaeed4@users.noreply.github.com>
2021-01-06 15:55:32 -0600

    push 9.0

--- comparisonOperators/compOutline.py
+++ comparisonOperators/compOutline.py
@@ -10,34 +10,42 @@ Fill in this outline as the instructor goes through the lesson.
 
 #1) Make two string variables. Compare them using the == operator and store
 #that comparison in a new variable. Then print the variable.
-
-
+z="blue"
+x="red"
+n=(z==x)
+print(n)
 
 #2) Make two int variables. Compare them using the == operator and store
 #that comparison in a new variable. Then print the variable.
-
-
-
+a=3
+b=3
+v=(a==b)
+print(v)
 #3) Make two int variables. Compare them using the > operator and store
 #that comparison in a new variable. Then print the variable.
-
-
-
+s=5
+d=8
+f=(s>d)
+print(f)
 #4) Make two int variables. Compare them using the < operator and store
 #that comparison in a new variable. Then print the variable.
-
-
+g=12
+h=2
+j=(g<h)
+print(j)
 
 #5) Make two int variables. Compare them using the >= operator and store
 #that comparison in a new variable. Then print the variable.
-
-
-
+k=9
+l=23
+q=(k>=l)
+print(q)
 #6) Make two int variables. Compare them using the <= operator and store
 #that comparison in a new variable. Then print the variable.
-
-
-
+c=52
+v=31
+m=(c<=v)
+print(m)
 #7) Make a comparison with any operator, with one of the common SYNTAX errors.
-
+>=5
 
